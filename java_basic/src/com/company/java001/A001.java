f7f003a9a2f71017d68030ffc865cef446d0a03c
tj-bu-703-01 <during4277@naver.com>
2025-02-17 17:57:07 +0900

    자바 1일차 commit

---
+++ java_basic/src/com/company/java001/A001.java
@@ -0,0 +1,19 @@
+package com.company.java001;
+
+public class A001 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	} //end main
+} // end class 
+
+//한줄 변경 ctrl  + alt + j
+// 한줄 수석(설명글)
+/* 3번째 줄 : public class A001 { 
+ 		public 아무데서나 접근가능 /	class	부품객체 / A001	클래스이름
+   4번째 줄 : jvm 구동시작점
+   	public static void main(String[] args) {
+	public 아무데서나 접근가능 / static 메모리상 사용가능 / 
+	void main(String[] args) 전원버튼 jvm이 찾는 메서드
+   5번째 줄 : System.out.println("Hello");
+   	System (운영체제) out ( cmd ) println(출력)
+ */
---
+++ java_basic/src/com/company/java001_ex/A002.java
@@ -0,0 +1,9 @@
+package com.company.java001_ex;
+
+public class A002 {
+
+	public static void main(String[] args) {
+		System.out.println("내 이름은 최영진입니다.");
+	}
+
+}
