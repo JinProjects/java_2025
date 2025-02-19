822d9b4a9fa864db96e7a02cfcbe915c81f828c8
tj-bu-703-01 <during4277@naver.com>
2025-02-19 17:52:48 +0900

    2025-02-19 BANK.java 추가

---
+++ java_basic/src/com/company/test/Bank.java
@@ -0,0 +1,26 @@
+package com.company.test;
+
+import java.util.Scanner;
+
+public class Bank {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int select;
+		System.out.println("=====BANK====");
+		System.out.println("* 1.추가");
+		System.out.println("* 2.조회");
+		System.out.println("* 3.입금");
+		System.out.println("* 4.출금");
+		System.out.println("* 5.삭제");
+		System.out.println("* 9.종료");
+		while(true) {
+//			if(select == 1) {
+				
+//			}
+		}
+		
+	}
+	
+}
